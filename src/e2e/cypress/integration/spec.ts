a8d1ff44620f6e9b9e5b5a815a7d142bc894ce12
Thiago Brezinski <thiagobrez@gmail.com>
2019-08-01 16:21:29 -0300

    test(e2e): add e2e tests for button component

--- src/e2e/cypress/integration/spec.ts
+++ src/e2e/cypress/integration/spec.ts
@@ -3,8 +3,10 @@ describe('Main E2E', () => {
     cy.visit('localhost:3000');
 
     const inputMessage = 'testing cypress';
+    const buttonMessage = 'Click here';
 
     cy.get('input').type(inputMessage);
     cy.get('p').should('have.text', inputMessage);
+    cy.get('[type="submit"]').should('have.text', buttonMessage);
   });
 });
